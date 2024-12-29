4b4143aa2f95cf8cc8f157be21e81c10fe5e5104
muhammedajmal953 <muhammedajmal953@gmail.com>
2024-12-29 17:35:50 +0530

    bugfixing

--- src/app/pages/user/view-hostel/view-hostel.component.ts
+++ src/app/pages/user/view-hostel/view-hostel.component.ts
@@ -101,11 +101,6 @@ export class ViewHostelComponent implements OnInit {
           timer: 1500,
           toast: true,
         });
-
-        if (err.error.message === 'Please login') {
-          localStorage.removeItem('user')
-          this._router.navigate(['/user'])
-        }
       },
       complete: () => {
         console.log('request completed');
